49d6c9602fd6253330a1c58b160f0ee8fb388f47
Jphsx <justin.anguiano1@gmail.com>
2018-10-23 20:02:45 +0200

    fixing file structure

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/desy.de/user/a/anguiano/work/repos/ILDbench_WWqqlnu
-# Build directory: /afs/desy.de/user/a/anguiano/work/repos/ILDbench_WWqqlnu/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(source)
