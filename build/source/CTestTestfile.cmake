463aa3df06078a156dbb0735d9233ae1f0050393
Jphsx <justin.anguiano1@gmail.com>
2018-10-23 19:44:42 +0200

    restructured scripts and added scripts for running or cleaning build/lib before push

---
+++ build/source/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /afs/desy.de/user/a/anguiano/work/repos/ILDbench_WWqqlnu/source
+# Build directory: /afs/desy.de/user/a/anguiano/work/repos/ILDbench_WWqqlnu/build/source
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
