43599daf2771485ce81b8c25567e5a8aae3cc34e
Jphsx <justin.anguiano1@gmail.com>
2018-12-12 17:13:28 -0600

    added most methods to jet variables

--- source/include/jetVariables.h
+++ source/include/jetVariables.h
@@ -9,17 +9,22 @@ using namespace lcio;
 class jetVariables{
 	public:
 
-	jetVariables(eventVariables*& evtvar);
+	jetVariables(eventVariables*& evtvar, LCEvent*& evt, std::string inputJetCollectionName);
 	void initLocalTree();
 
+	LCEvent* _localEvt{};
+	std::string _inputJetCollectionName{};
 	eventVariables* _evtvar{};
 	TTree* _localTree{};
 	//local jets copy
+	const char* _variableSetName{};
+	unsigned int _nJets{};
 	std::vector<ReconstructedParticle*> _jets{};
 	std::vector<TLorentzVector*> _tlvjets{};
 
-	double _logym{};
-	double _logyp{};
+	double _logyMinus{};
+	double _logyPlus{};
+	std::vector<double> _jetMinPsi{}
 
 	//MC tag jet variables
 	int _mctlepPfoMult{};
@@ -28,12 +33,22 @@ class jetVariables{
 	int _mctDwnPfoMult{};
 	int _mctUpTrkMult{};
 	int _mctDwnTrkMult{};
-	std::vector<double> mctjetMinPsi{}; 
+
 
 	//analysis jet variables
 	int _analepPfoMult{};
 	int _analepTrkMult{};
-	int _anajetMinPsi{};
+
+
+
+	void setLogYVariables(double& logyMinus, double& logyPlus);
+
+	void setMCTJetMultiplicity(int& mctlepPfoMult, int& mctlepTrkMult, int& mctUpPfoMult, int& mctDwnPfoMult, int& mctUpTrkMult, int& mctDwnTrkMult);
+
+	void setMCTMinPsi(std::vector<double>& minPsi);
+	
+	void setAnaJetMultiplicity();
+
 
 };
 #endif
--- source/src/jetVariables.cc
+++ source/src/jetVariables.cc
@@ -1,10 +1,81 @@
 #include "jetVariables.h"
 
 
-jetVariables::jetVariables(eventVariables*& evtvar){
+jetVariables::jetVariables(eventVariables*& evtvar, LCEvent*& evt, std::string inputJetCollectionName){
 	_evtvar=evtvar;
 	_localTree = evtvar->_localTree;
 
+	_localEvt = evt;
+	_inputJetCollectionName = inputJetCollectionName;
+
 	//init rest of local stuff
+	_variableSetName = evtvar->_variableSetName;
+	_nJets = evtvar->_nJets;
+	_jets = evtvar->_jets;
+	_tlvjets = evtvar->_tlvjets;
+
+	//init vector size
+	std::vector<double> temp(_nJets);
+ 	_jetMinPsi = temp; 
+
 
 }
+void jetVariables::getLogYVariables(double& logyMinus, double& logyPlus){
+	yMinus =(double) std::log( evt->getCollection(_inputJetCollectionName)->getParameters().getFloatVal( "y_{n-1,n}" ));
+    yPlus  =(double) std::log(evt->getCollection(_inputJetCollectionName)->getParameters().getFloatVal( "y_{n,n+1}" ));
+}
+void jetVariables::setMCTJetMultiplicity( int& mctlepPfoMult, int& mctlepTrkMult, int& mctUpPfoMult, int& mctDwnPfoMult, int& mctUpTrkMult, int& mctDwnTrkMult ){
+
+	//mctagged jets must already be populated in evt variables
+
+	std::vector<int> _localmctTags = _evtvar->_jetmctags;
+
+	std::vector<int> _pfoMult(_nJets);
+	std::vector<int> _trkMult(_nJets);
+	int pfoCount = 0;
+	int trkCount = 0;
+	//first loop over each jet and get charge and pfo multiplicities
+	for(unsigned int i=0; i<_jets.size(); i++){
+		std::vector<ReconstructedParticle*> _jetParts = _jets.at(i)->getParticles();
+		for(unsigned int j=0; j< _jetparts.size(); j++){
+			if( _jetparts.at(i)->getCharge() != 0){
+				trkCount++;
+			}
+			pfoCount++;
+		}
+		_pfoMult.at(i) = pfoCount;
+		_trkMult.at(i) = trkCount;
+		pfoCount = 0;
+		trkCount = 0;
+	}
+	
+	//loop over the tags and assign mults to corresponding variables
+	for(unsigned int i=0; i< _localmctTags.size(); i++){
+		//is it a quark?
+		if( abs(_localmctTags.at(i)) < 6 ){
+			//is it uplike or downlike?
+			if( abs(_localmctTags.at(i)) % 2 == 0){
+				//its uplike
+				mctUpTrkMult = _trkMult.at(i);
+				mctUpPfoMult = _pfoMult.at(i);
+			}
+			else{
+				//its downlike
+				mctDwnTrkMult = _trkMult.at(i);
+				mctDwnPfoMult = _pfoMult.at(i);
+			}
+		}
+		else{
+		//its a lepton
+			mctlepTrkMult = _trkMult.at(i);
+			mctlepPfoMult = _pfoMult.at(i);
+		}
+
+	}
+
+}
+void jetVariables::setMinPsi(std::vector<double>& minPsi){
+		
+
+}
+	
