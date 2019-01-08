37ad173af476fbcbe7afa7a9e8e787d10538a9a8
Jphsx <justin.anguiano1@gmail.com>
2019-01-08 17:42:25 -0600

    debug

--- source/include/ReconstructedOverlayRemoval.h
+++ source/include/ReconstructedOverlayRemoval.h
@@ -1,7 +1,7 @@
 #include "marlin/Processor.h"
 #include "EVENT/MCParticle.h"
 #include "EVENT/ReconstructedParticle.h"
-#include "EVENT/LCCollection.h"
+#include "EVENT/LCCollectionVec.h"
 #include "EVENT/LCRelation.h"
 #include "lcio.h"
 #include "TFile.h"
