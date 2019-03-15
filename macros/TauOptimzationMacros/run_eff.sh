390c720902f28d00c024a71184865aa183d57238
Jphsx <justin.anguiano1@gmail.com>
2019-03-15 15:13:41 -0500

    added eff sh

--- macros/TauOptimzationMacros/run_eff.sh
+++ macros/TauOptimzationMacros/run_eff.sh
@@ -5,18 +5,19 @@
 #PARTICLETYPE = ['MUON', 'ELECTRON', 'TAU0', 'TAU1', 'TAU2', 'TAU3', 'TAU4', 'BG1']
 
 
-SUBSET = "S1"
-PTYPE = "MUON"
-BTYPE = "B1"
-nfiles = 39
-ntrees = 50
+SUBSET="S1"
+PTYPE="MUON"
+BTYPE="B1"
+nfiles=39
+ntrees=50
+
 root -l
 .x Efficiency_Rejection.C(${SUBSET},${PTYPE},${BTYPE},${nfiles},${ntrees})
 .q
 
 
-SUBSET = "S1"
-PTYPE = "TAU0"
+SUBSET="S1"
+PTYPE="TAU0"
 root -l
 .x Efficiency_Rejection.C(${SUBSET},${PTYPE},${BTYPE},${nfiles},${ntrees})
 .q
