e7a8a58369f37eb54f58af41f00d060102898b10
Jphsx <justin.anguiano1@gmail.com>
2019-03-15 14:41:18 -0500

    debug

--- macros/TauOptimzationMacros/Efficiency_Rejection.C
+++ macros/TauOptimzationMacros/Efficiency_Rejection.C
@@ -264,7 +264,7 @@ void Efficiency_Rejection(const char* subsetTag, const char* particletypeTag , c
 
 			//loop signal tree
 			auto nevent = tree->GetEntries();
-			nevent = 1000;
+			//nevent = 1000;
 
 			//zero counts
 			Total_s = 0.;
@@ -273,11 +273,11 @@ void Efficiency_Rejection(const char* subsetTag, const char* particletypeTag , c
       				tree->GetEvent(i);		
 
 					if( !passAcceptance( MCf0, MCf1, MCf2, MCf3, MCf0_PDG, MCf1_PDG, MCf2_PDG, MCf3_PDG) ){
-						std::cout<<"FAIL ";
+						//std::cout<<"FAIL ";
 					 continue;
 					}
 					
-						std::cout<<"PASS "<<std::endl;
+						//std::cout<<"PASS "<<std::endl;
 					
 		
 				
@@ -327,10 +327,7 @@ void Efficiency_Rejection(const char* subsetTag, const char* particletypeTag , c
 				
 				}
 			}
-		//	currentFile->Close();
-		//	currentFile = TFile::Open(infbg.str().c_str());
-		//	tree = (TTree*)currentFile->Get(treeNames.at(itree).c_str());
-			//currentB
+		
 			//redirect other tree to same vars
  			treebg->SetBranchAddress((bgtreeNames.at(itree)+"MCf0").c_str(), &MCf0, &bMCf0);
 			treebg->SetBranchAddress((bgtreeNames.at(itree)+"MCf1").c_str(), &MCf1, &bMCf1);
@@ -343,421 +340,50 @@ void Efficiency_Rejection(const char* subsetTag, const char* particletypeTag , c
 			treebg->SetBranchAddress((bgtreeNames.at(itree)+"nTaus").c_str(), &nTaus, &bnTaus);
 
 			//loop over bg tree
+			//zero counts
+			Total_b = 0.;
+			N_b = 0.;
 			
 			nevent = treebg->GetEntries();
-			nevent = 40;
+	//		nevent = 40;
    			for (Int_t i=0;i<nevent;i++) {
       				treebg->GetEvent(i);		
 					if( !passAcceptance( MCf0, MCf1, MCf2, MCf3, MCf0_PDG, MCf1_PDG, MCf2_PDG, MCf3_PDG) ){
-						std::cout<<"FAIL ";
+						//std::cout<<"FAIL ";
 					 continue;
 					}
-					
-					std::cout<<"PASS "<<std::endl;
-					
+					//std::cout<<"PASS "<<std::endl;
+					Total_b++;
+					if(nTaus >= 1){
+						N_b++;
+					}
 					
 			}
 			
 
 			eff_s = N_s/Total_s;
-			//eff_b = N_b/Total_b;
-			//RR = 1. - eff_b;
+			eff_b = N_b/Total_b;
+			RR = 1. - eff_b;
 			treeN = treedetails.at(treeDetailsItr).at(0);
 			searchCone = treedetails.at(treeDetailsItr).at(1);
 			isoCone = treedetails.at(treeDetailsItr).at(2);
 			isoE = treedetails.at(treeDetailsItr).at(3);
-			//p = N_s/ (N_s+N_b);
-			//effp = eff_s * p;
+			p = N_s/ (N_s+N_b);
+			effp = eff_s * p;
 				 
 			outputTree->Fill();
 	
 			treeDetailsItr++;
-			break;
+			
 
-		}	
+		}//end tree loop	
 
 		
-	}
+	}//end file loop
 	outputFile->Write();
 }
 
-/*
-def bash( bashCommand ):
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	#process = subprocess.Popen(bashCommand.split())
-	output, error = process.communicate()
-	return output ,error
-def GetKeyNames( self, dir = "" ):
-        self.cd(dir)
-        return [key.GetName() for key in gDirectory.GetListOfKeys()]
-TFile.GetKeyNames = GetKeyNames
 
-def GetTreeObject(tree, branchName ,value):
-	branchesObj = tree.GetListOfBranches()
-	branches = []
-	for b in branchesObj:
-		branches.append( b.GetName() )
-		
-#	print "got branches"
-	#print branches
-	branch = [b for b in branches if branchName in b]
-	#print "got branch"
-	#print branch
-	#print "done"
-	tree.SetBranchAddress(branch[0],value)
-	#return branch.GetEntries()
-	#ranch.GetLeafEntry
-	#leafs = tree.GetListOfLeaves()
-	#print leafs
-
-def makearray(mini,Maxi,step):
-	#start = mini
-	arr = []
-	while mini < Maxi :
-		arr.append(mini)
-		mini = mini + step
-		mini = round(mini, 4)
-	return arr
-
-def getmaxenergy( ntaus, vec4):
-	if ntaus == 0:
-		return 0.
-	maxE = 0.
-	for v in vec4:
-		if v.E() > maxE:
-			maxE = v.E()
-
-	return maxE
-
-def passAcceptance( flist, fpdglist):
-	passes = True
-	for mcf, pdg in zip(flixt, fpdglist):
-		if abs(pdg) != 12 and abs(pdg) != 14 and abs(pdg) != 16:
-			#no neutrinos allowed
-			if abs( mcf.CosTheta() > 0.995 ):
-				passes = False
-	return passes 
-
-#run over 3 subsets S1, S2, B1
-
-#on run define what subset
-SUBSET = ['S1', 'S2', 'B1']
-PARTICLETYPE = ['MUON', 'ELECTRON', 'TAU0', 'TAU1', 'TAU2', 'TAU3', 'TAU4', 'BG1']
-
-#CHOOSE SUBSET/PTYPE INDICES
-BGSUBSET = SUBSET[2]
-BGPARTICLETYPE = PARTICLETYPE[7]
-SUBSET = SUBSET[0]
-PARTICLETYPE = PARTICLETYPE[0]
-
-
-
-#Since i didnt store it, figure out the parameters for each tree and store them with the eff
-searchConeAngleMin = 0.
-searchConeAngleMax = 0.16
-searchConeAngleStep = 0.01 #10mrad step
-
-isoAngleMin = 0.
-isoAngleMax = 0.11
-isoAngleStep = 0.01 #10mrad step
-
-isoEnergyMin = 0.
-isoEnergyMax = 11.
-isoEnergyStep = 1. #1gev step
-
-#number of trees per file
-#nTreesPerFile = 50
-
-
-#create the parameter sets
-coneValues = makearray(searchConeAngleMin, searchConeAngleMax, searchConeAngleStep)
-isoAngleValues = makearray(isoAngleMin, isoAngleMax, isoAngleStep)
-isoEnergyValues = makearray(isoEnergyMin, isoEnergyMax, isoEnergyStep)
-
-#make a little sublist of [tree number, cone angle, iso angle, iso energy]
-treedetails = []
-treenum = 1
-for c in coneValues:
-	for iA in isoAngleValues:
-		for iE in isoEnergyValues:
-			treedetails.append( [ treenum, c, iA, iE ])
-			treenum = treenum + 1
-
-#print treedetails
-
-LS = bash('ls /nfs/dust/ilc/user/anguiano/WWBenchmark/WWFiles/TauOptimizationFiles/RootFiles/')
-LS = LS[0].split('\n')
-
-#collect the files of chosen subset
-FILESUBSET = [f for f in LS if SUBSET in f]
-BGFILESUBSET = [f for f in LS if BGSUBSET in f]
-
-#print FILESUBSET
-#print BGFILESUBSET
-
-#we need to write all this stuff to a tree in a rootfile
-outputFile = TFile.Open('/nfs/dust/ilc/user/anguiano/WWBenchmark/WWFiles/TauOptimizationFiles/EffRootFiles/'+SUBSET+PARTICLETYPE+'.root','RECREATE')
-outputTree = TTree(SUBSET+PARTICLETYPE, SUBSET+PARTICLETYPE)
-eff_s, eff_b, RR, treeN, searchCone, isoCone, isoE, p, effp, N_s, N_b, Total_s, Total_b = array( 'f', [ 0. ] ),array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) ,array( 'f', [ 0. ]) 
-#eff_sL, eff_bL, RRL, treeNL, searchConeL, isoConeL, isoEL, pL, effpL, N_sL, N_bL, Total_sL, Total_bL = [], [], [], [], [], [], [], [], [], [], [], [], []
-Emax_s, Emax_b = array( 'f', [0.] ), array( 'f', [0.] )
-#set branches
-outputTree.Branch("eff_s",eff_s,"eff_s/F")
-outputTree.Branch("eff_b",eff_b,"eff_b/F")
-outputTree.Branch("RR",RR,"RR/F")
-outputTree.Branch("treeN",treeN,"treeN/F")
-outputTree.Branch("searchCone",searchCone,"searchCone/F")
-outputTree.Branch("isoCone",isoCone,"isoCone/F")
-outputTree.Branch("isoE",isoE,"isoE/F")
-outputTree.Branch("p",p,"p/F")
-outputTree.Branch("effp",effp,"effp/F")
-outputTree.Branch("N_s",N_s,"N_s/F")
-outputTree.Branch("N_b",N_b,"N_b/F")
-outputTree.Branch("Total_s", Total_s,"Total_s/F")
-outputTree.Branch("Total_b", Total_b,"Total_b/F")
-#outputTree.Branch("Emax_s", Emax_s,"Emax_s/F") #the higheest energy tau reconstucted
-#outputTree.Branch("Emax_b", Emax_b,"Emax_b/F")
-
-#iterator for tree details
-treedetails_itr = 0;
-#combine FILESUBSETS
-#FILESUPERSET = []
-#for f1 in FILESUBSET:#
-#	FILESUPERSET.append(f1)
-#for b1 in BGFILESUBSET:
-#	FILESUPERSET.append(b1)
-
-#FILESUBSET = FILESUPERSET
-#tree loop vars
-isMuon = array('i',[0])
-isTau = array('i',[0])
-isElectron = array('i',[0])
-tauType = array('i',[0])
-nTaus = array('i',[0])
-nTausBG = array('i',[0])
-#tauTLV = rt.vector('TLorentzVector')()
-#tauTLVBG = rt.vector('TLorentzVector')()
-
-#look at acceptance
-#currently we only look at 4f events
-mcf0 = TLorentzVector()
-mcf1 = TLorentzVector()
-mcf2 = TLorentzVector()
-mcf3 = TLorentzVector()
-mcf0_pdg = array('i',[0])
-mcf1_pdg = array('i',[0])
-mcf2_pdg = array('i',[0])
-mcf3_pdg = array('i',[0])
-mcf0bg = TLorentzVector()
-mcf1bg = TLorentzVector()
-mcf2bg = TLorentzVector()
-mcf3bg = TLorentzVector()
-mcf0_pdgbg = array('i',[0])
-mcf1_pdgbg = array('i',[0])
-mcf2_pdgbg = array('i',[0])
-mcf3_pdgbg = array('i',[0])
-
-
-
-
-
-print FILESUBSET
-print BGFILESUBSET
-#loop over the list of files and collect all the trees
-for filename, filenameBG in zip(FILESUBSET, BGFILESUBSET):
-	currentFile = TFile.Open('/nfs/dust/ilc/user/anguiano/WWBenchmark/WWFiles/TauOptimizationFiles/RootFiles/'+filename)
-	currentBGFile = TFile.Open('/nfs/dust/ilc/user/anguiano/WWBenchmark/WWFiles/TauOptimizationFiles/RootFiles/'+filenameBG)	
-
-	keyList = currentFile.GetKeyNames('./')
-	print "\nKeys in file:", keyList
-	treeNames = currentFile.GetKeyNames('./')
-	treeNamesBG = currentBGFile.GetKeyNames('./')
-	#extract each tree
-	for tn,tnbg in zip(treeNames, treeNamesBG):
-		tree = currentFile.Get(tn)
-		treebg = currentBGFile.Get(tnbg)
-
-		GetTreeObject(tree, 'isMuon', isMuon )
-		GetTreeObject(tree, 'nTaus', nTaus )
-		GetTreeObject(tree, 'isTau', isTau)
-		GetTreeObject(tree, 'isElectron', isElectron)
-		GetTreeObject(tree, 'tauType', tauType)
-		GetTreeObject(treebg, 'nTaus', nTausBG)
-	#	GetTreeObject(tree, 'tauTLV', tauTLV)
-	#	GetTreeObject(treebg, 'tauTLV', tauTLVBG) 
-		GetTreeObject(tree, 'MCf0', mcf0)
-		GetTreeObject(tree, 'MCf1', mcf1)
-		GetTreeObject(tree, 'MCf2', mcf2)
-		GetTreeObject(tree, 'MCf3', mcf3)
-		GetTreeObject(tree, 'MCf0_PDG', mcf0_pdg)
-		GetTreeObject(tree, 'MCf1_PDG', mcf1_pdg)
-		GetTreeObject(tree, 'MCf2_PDG', mcf2_pdg)
-		GetTreeObject(tree, 'MCf3_PDG', mcf3_pdg)
-		GetTreeObject(tree, 'MCf0', mcf0bg)
-		GetTreeObject(tree, 'MCf1', mcf1bg)
-		GetTreeObject(tree, 'MCf2', mcf2bg)
-		GetTreeObject(tree, 'MCf3', mcf3bg)
-		GetTreeObject(tree, 'MCf0_PDG', mcf0_pdgbg)
-		GetTreeObject(tree, 'MCf1_PDG', mcf1_pdgbg)
-		GetTreeObject(tree, 'MCf2_PDG', mcf2_pdgbg)
-		GetTreeObject(tree, 'MCf3_PDG', mcf3_pdgbg)
-		
-		#for entry, entryBG in zip(tree, treebg):
-		for entry in tree:
-			mcflist = [ mcf0, mcf1, mcf2, mcf3 ]
-			mcfpdglist = [mcf0_pdg, mcf1_pdg, mcf2_pdg, mcf3_pdg ]
-			if passAcceptance(mcflist, mcfpdglist) == False:
-				continue
-
-			#does entry pass mc acceptance
-			if isMuon[0] and PARTICLETYPE == 'MUON':
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				#print nTaus
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-		#print N_s[0], Total_s[0]
-			if isTau[0] and PARTICLETYPE == 'TAU0' and tauType[0] == 0:
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-			if isTau[0] and PARTICLETYPE == 'TAU1' and tauType[0] == 1:
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-			if isTau[0] and PARTICLETYPE == 'TAU2' and tauType[0] == 2:
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-			if isTau[0] and PARTICLETYPE == 'TAU3' and tauType[0] == 3:
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-			if isTau[0] and PARTICLETYPE == 'TAU4' and tauType[0] == 4:
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-			if isElectron[0] and PARTICLETYPE == 'ELECTRON':
-				Total_s[0] = Total_s[0]+1.
-				round(Total_s[0])
-				if nTaus[0] == 1:
-					N_s[0] = N_s[0] + 1.
-					round(N_s[0])
-
-		for entry in treebg:
-			mcfbglist = [ mcf0bg, mcf1bg, mcf2bg, mcf3bg ]
-			mcfpdgbglist = [mcf0_pdgbg, mcf1_pdgbg, mcf2_pdgbg, mcf3_pdgbg ]
-			if passAcceptance(mcfbglist, mcfpdgbglist) == False:
-				continue
-
-			if True: #this is background
-				Total_b[0] = Total_b[0]+1.
-				round(Total_b[0])
-				#print nTausBG[0]
-				if nTausBG[0] >= 1:
-					N_b[0] = N_b[0] + 1.
-					round(N_b[0])
-		
-		eff_s[0] = (N_s[0]/Total_s[0])
-		treeN[0] =(treedetails[treedetails_itr][0])
-		searchCone[0] = (treedetails[treedetails_itr][1])
-		isoCone[0] = (treedetails[treedetails_itr][2])
-		isoE[0] =(treedetails[treedetails_itr][3])			
-		eff_b[0] =(N_b[0]/Total_b[0])
-		RR[0] = (1. - eff_b[0])
-		p[0] = (N_s[0] / (N_s[0] + N_b[0]))
-		effp[0] = eff_s[0] * p[0]
-	#	Emax_s[0] =getmaxenergy( nTaus[0], tauTLV)
-	#	Emax_b[0] =getmaxenergy( nTausBG[0], tauTLVBG)
-		print eff_s[0], eff_b[0], RR[0], treeN[0], searchCone[0], isoCone[0], isoE[0], p[0], effp[0], N_s[0], N_b[0], Total_s[0], Total_b[0] #, Emax_s[0], Emax_b[0]
-		outputTree.Fill()
-		treedetails_itr += 1
-		
-	
-		#loop over the tree
-		#for nevents in tree:
-
-		#	GetTreeObject(tree, 'isMuon' )
-		#	break
-	#		if PARTICLETYPE == 'MUON':
-	#			#if nevent.(tn+'isMuon'):
-	#			if 0:
-	#				Total_s = Total_s+ 1.
-	#			#	if nevent.(tn+'nTaus') == 1:
-	#			if 0:
-	#					N_s = N_s + 1.
-	#				
-	#		if PARTICLETYPE == 'ELECTRON':
-	#			True
-#
-#			if PARTICLETYPE == 'TAU0':
-#				True
-#
-#			if PARTICLETYPE == 'TAU1':
-#				True
-##			if PARTICLETYPE == 'TAU2':
-#				True
-#
-#			if PARTICLETYPE == 'TAU3':
-#				True
-#	
-#			if PARTICLETYPE == 'TAU4':
-#				True
-
-		#populate variables according to the current tree and fill our output tree
-#		eff_sL.append(N_s/Total_s)
-#		treeNL.append(treedetails[treedetails_itr][0])
-#		searchConeL.append(treedetails[treedetails_itr][1])
-#		isoConeL.append(treedetails[treedetails_itr][2])
-#		isoEL.append(treedetails[treedetails_itr][3])
-		#tree.Fill()
-
-#we have to loop over the bg file separate
-#for filename in BGFILESUBSET:
-#	currentFile = TFile.Open(filename)
-#	treeNames = currentFile.GetKeyNames('./')
-	#extract each tree
-#	for tn in treeNames:
-#		tree = currentFile.Get(tn)
-		#loop over the tree
-#		for nevents in tree:
-#			Total_b = Total_b + 1.
-		#if nevent.(tn+'nTaus') :
-#		if 0:
-#				N_b = N_b + 1.
-#
-#		eff_bL.append(N_b/Total_b)
-#		RRL.append(1. - eff_b)
-#		pL.append(N_s / (N_s + N_b))
-#		effPL.append(1.)
-		
-#loop over everything
-#for A,B,C,D,E,F,G,H,I,J,K,L,M in zip(eff_sL, eff_bL, RRL, treeNL, searchConeL, isoConeL, isoEL, pL, effpL, N_sL, N_bL, Total_sL, Total_bL):
-#	eff_s, eff_b, RR, treeN, searchCone, isoCone, isoE, p, effp, N_s, N_b, Total_s, Total_b = A,B,C,D,E,F,G,H,I,J,K,L,M
-#	effp = eff_s * p
-#	outputTree.Fill()
-	
-
-outputFile.Write()
-		
-*/
 
 
 
