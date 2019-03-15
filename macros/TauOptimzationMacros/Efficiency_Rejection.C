cf95a625327a077c0a18b7faca1e453208716b50
Jphsx <justin.anguiano1@gmail.com>
2019-03-15 16:47:33 -0500

    debugging 39

--- macros/TauOptimzationMacros/Efficiency_Rejection.C
+++ macros/TauOptimzationMacros/Efficiency_Rejection.C
@@ -198,7 +198,7 @@ void Efficiency_RejectionRun(const char* subsetTag, const char* particletypeTag 
 	std::string inpath = "/nfs/dust/ilc/user/anguiano/WWBenchmark/WWFiles/TauOptimizationFiles/RootFiles/";
 	//loop over files
 	//for( int ifile=0; ifile<filenames.size(); ifile++){// filenameBG in zip(FILESUBSET, BGFILESUBSET):
-	for( int ifile=38; ifile<filenames.size(); ifile++){	
+	for( int ifile=37; ifile<filenames.size(); ifile++){	
 		std::stringstream inf;
 		inf<< inpath << filenames.at(ifile);
 		TFile* currentFile =  TFile::Open(inf.str().c_str());
