86b556a8a7768677f43f831963fa6a3ab7c234d7
Jphsx <justin.anguiano1@gmail.com>
2019-05-21 12:09:25 -0500

    debug

--- source/src/jetVariables.cc
+++ source/src/jetVariables.cc
@@ -31,7 +31,7 @@ void jetVariables::setLogYVariables(double& logyMinus, double& logyPlus){
 	logyMinus =(double) std::log( _localEvt->getCollection(_inputJetCollectionName)->getParameters().getFloatVal( "y_{n-1,n}" ));
     logyPlus  =(double) std::log( _localEvt->getCollection(_inputJetCollectionName)->getParameters().getFloatVal( "y_{n,n+1}" ));
 }
-void jetVariables::setParticles(std::vector<ReconstructedParticle*> jets, LCEvent* evt){
+void jetVariables::setParticles(std::vector<ReconstructedParticle*>& jets, LCEvent* evt){
 	std::cout<<" inside set parts"<<std::endl;
 	_jets = jets;
 	std::cout<<"jets size "<< _jets.size()<<std::endl;
